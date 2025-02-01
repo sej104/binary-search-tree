b3b1ce99167a733ff7f72081bd8373e6f07067b7
Sergio Joya <sejoya104@gmail.com>
2025-02-01 14:21:33 -0500

    Add test file

---
+++ main.js
@@ -0,0 +1,18 @@
+import Tree from "./Tree.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const tree = new Tree(array);
+prettyPrint(tree.root);
