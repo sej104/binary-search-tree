d08a579a410edfcdc787e174325c7f39329e29c6
Sergio Joya <sejoya104@gmail.com>
2025-02-04 13:42:59 -0500

    Write driver script

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 import Tree from "./Tree.js";
+import Node from "./Node.js";
+
+const generateArray = (length) => {
+  const array = [];
+  for (let i = 0; i < length; i++) {
+    array.push(Math.floor(Math.random() * 100));
+  }
+  return array;
+};
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
   if (node === null) {
@@ -13,6 +22,28 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 
-const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const array = generateArray(7);
 const tree = new Tree(array);
+
 prettyPrint(tree.root);
+console.log(tree.isBalanced());
+
+// tree.levelOrder((node) => console.log(node.data));
+// tree.preOrder((node) => console.log(node.data));
+// tree.inOrder((node) => console.log(node.data));
+// tree.postOrder((node) => console.log(node.data));
+
+tree.insert(150);
+tree.insert(100);
+tree.insert(500);
+prettyPrint(tree.root);
+console.log(tree.isBalanced());
+
+tree.rebalance();
+prettyPrint(tree.root);
+console.log(tree.isBalanced());
+
+// tree.levelOrder((node) => console.log(node.data));
+// tree.preOrder((node) => console.log(node.data));
+// tree.inOrder((node) => console.log(node.data));
+// tree.postOrder((node) => console.log(node.data));
